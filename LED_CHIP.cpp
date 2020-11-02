6a56fc7ca5adebffbe5c416cb57b5e2896a8a40c
neythen <zcqsntr@ucl.ac.uk>
2020-11-02 12:10:30 +0000

    data structures

--- LED_CHIP.cpp
+++ LED_CHIP.cpp
@@ -1,6 +1,24 @@
 
 
-int main() {
 
+class LEDchip {
 
-}
+  public:
+    unsigned char memory[4096];
+
+    unsigned char data_registers[16];
+    unsigned short address_register;
+
+    unsigned short program_counter;
+    unsigned short stack_pointer;
+    unsigned short stack[16];
+
+    unsigned char pixels[64 * 32];
+    unsigned short opcode;
+
+    unsigned char delay_timer;
+    unsigned char sound_timer;
+
+    unsigned char keys[16];
+
+};
